849db3fa93655583341cfde71a116d784d65499f
Fernando Junior <nandojuniordev@gmail.com>
2023-07-18 14:20:30 -0300

    feat: Project Finished

---
+++ src/components/Chat.tsx
@@ -0,0 +1,72 @@
+"use client";
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+  CardFooter,
+} from "@/components/ui/card";
+
+import { useChat } from "ai/react";
+import { ScrollArea } from "./ui/scroll-area";
+
+export function Chat() {
+  const { messages, input, handleInputChange, handleSubmit } = useChat({
+    api: "/api/chat",
+  });
+
+  return (
+    <Card className="w-[440px]">
+      <CardHeader>
+        <CardTitle>Chat AI 🤖</CardTitle>
+        <CardDescription>
+          Using Vercel SDK to create a chat bot.
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <ScrollArea className="h-[600px] w-full pr-4">
+          {messages.map((message) => (
+            <div
+              className="flex gap-3 text-slate-600 text-sm mb-4"
+              key={message.id}
+            >
+              {message.role === "user" && (
+                <Avatar>
+                  <AvatarFallback>FJ</AvatarFallback>
+                  <AvatarImage src="https://github.com/nandobfr.png" />
+                </Avatar>
+              )}
+              {message.role === "assistant" && (
+                <Avatar>
+                  <AvatarFallback>AI</AvatarFallback>
+                  <AvatarImage src="https://github.com/cyberlabsai.png" />
+                </Avatar>
+              )}
+              <p className="leading-relaxed">
+                <span className="block font-bold text-slate-700">
+                  {message.role === "user" ? "User:" : "AI:"}
+                </span>
+                {message.content}
+              </p>
+            </div>
+          ))}
+        </ScrollArea>
+      </CardContent>
+      <CardFooter>
+        <form className="w-full flex gap-2" onSubmit={handleSubmit}>
+          <Input
+            placeholder="How can I help you?"
+            value={input}
+            onChange={handleInputChange}
+          />
+          <Button type="submit">Send</Button>
+        </form>
+      </CardFooter>
+    </Card>
+  );
+}
